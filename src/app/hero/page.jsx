ae22fb36a5d02bcb80b1c75571ca2997d15e04cb
JowelislamHabib <jowel@bintofajjal.com>
2026-04-20 11:26:44 +0600

    first commit

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@heroui/react";
+import React from "react";
+
+const HeroPage = () => {
+  return (
+    <div>
+      <h1>Hero Page</h1>
+      <Button>Hero button</Button>
+      <Button variant="secondary">Hero button</Button>
+      <Button variant="danger">Hero button</Button>
+    </div>
+  );
+};
+
+export default HeroPage;
