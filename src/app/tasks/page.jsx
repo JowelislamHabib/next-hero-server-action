b71d55872a5e023c95a5905d27b7d01694efbadd
JowelislamHabib <jowel@bintofajjal.com>
2026-04-20 13:48:01 +0600

    44-5 Introduction to server actions

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,5 +1,6 @@
 import { AddTasks } from "@/components/AddTask";
 import TaskCard from "@/components/TaskCard";
+import { createATask } from "@/lib/actions";
 import { getTasks } from "@/lib/tasks";
 import React from "react";
 
@@ -8,7 +9,7 @@ const TaskPage = async () => {
   return (
     <div>
       <h2 className="py-4">Tasks: {tasks.length}</h2>
-      <AddTasks />
+      <AddTasks createATask={createATask} />
       <div className="grid grid-cols-3 gap-4 mt-10">
         {tasks.map((task) => (
           <TaskCard task={task} key={task.id} />
