bff6305b4d2e62b33a19a70b32d4425882f97e0a
JowelislamHabib <jowel@bintofajjal.com>
2026-04-20 13:04:34 +0600

    44-4 Explore switch theme toggle and modal

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,3 +1,4 @@
+import { AddTasks } from "@/components/AddTask";
 import TaskCard from "@/components/TaskCard";
 import { getTasks } from "@/lib/tasks";
 import React from "react";
@@ -6,8 +7,9 @@ const TaskPage = async () => {
   const tasks = await getTasks();
   return (
     <div>
-      <h2>Tasks: {tasks.length}</h2>
-      <div className="grid grid-cols-3 gap-4">
+      <h2 className="py-4">Tasks: {tasks.length}</h2>
+      <AddTasks />
+      <div className="grid grid-cols-3 gap-4 mt-10">
         {tasks.map((task) => (
           <TaskCard task={task} key={task.id} />
         ))}
