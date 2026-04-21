34f385fdb3583c0c777b602849776057e2922811
JowelislamHabib <jowel@bintofajjal.com>
2026-04-21 13:54:36 +0600

    44-7 Revalidation after server action to display form data

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,5 +1,21 @@
+import { revalidatePath } from "next/cache";
+import { postTask } from "./tasks";
 export const createATask = async (formData) => {
   "use server";
-  const name = formData.get("name");
-  console.log("task form", formData);
+  // const name = formData.get("name");
+  // const title = formData.get("title");
+  // const description = formData.get("description");
+  // const priority = formData.get("priority");
+  // const status = formData.get("status");
+  // const assignedTo = formData.get("assignedTo");
+
+  // const newTask = { title, description, priority, status, assignedTo, name };
+
+  const newTask = Object.fromEntries(formData.entries());
+
+  console.log("task form", newTask);
+  const res = await postTask(newTask);
+  if (res.success) {
+    revalidatePath("/tasks");
+  }
 };
